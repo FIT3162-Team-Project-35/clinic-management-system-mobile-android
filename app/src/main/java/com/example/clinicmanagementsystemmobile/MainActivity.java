b51c7898c312450516dc4a29e7e57395333b0fb2
hyaoying <yhua0144@student.monash.edu>
2022-10-06 17:41:37 +0800

    access camera to the app

--- app/src/main/java/com/example/clinicmanagementsystemmobile/MainActivity.java
+++ app/src/main/java/com/example/clinicmanagementsystemmobile/MainActivity.java
@@ -1,48 +1,282 @@
+//package com.example.clinicmanagementsystemmobile;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.graphics.Bitmap;
+//import android.os.Bundle;
+//import android.webkit.WebSettings;
+//import android.webkit.WebView;
+//import android.webkit.WebViewClient;
+//
+//public class MainActivity extends AppCompatActivity {
+//    private WebView mywebView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mywebView=(WebView) findViewById(R.id.webview);
+//        mywebView.setWebViewClient(new WebViewClient());
+//        WebSettings settings = mywebView.getSettings();
+//        settings.setDomStorageEnabled(true);
+//        mywebView.loadUrl("https://clinic-management-system-fe.vercel.app/");
+//        WebSettings webSettings=mywebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//    }
+//
+//    public class mywebClient extends WebViewClient{
+//        @Override
+//        public void onPageStarted(WebView view, String url, Bitmap favicon){
+//            super.onPageStarted(view,url,favicon);
+//        }
+//        @Override
+//        public boolean shouldOverrideUrlLoading(WebView view,String url){
+//            view.loadUrl(url);
+//            return true;
+//        }
+//    }
+//    @Override
+//    public void onBackPressed(){
+//        if(mywebView.canGoBack()) {
+//            mywebView.goBack();
+//        }
+//        else{
+//            super.onBackPressed();
+//        }
+//    }
+//}
+
 package com.example.clinicmanagementsystemmobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+//import android.support.annotation.RequiresApi;
+import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
-    private WebView mywebView;
+import androidx.annotation.RequiresApi;
 
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    private WebView webview;
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mUploadMessages;
+    private Uri mCapturedImageURI = null;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int FILECHOOSER_RESULTCODE = 1;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mywebView=(WebView) findViewById(R.id.webview);
-        mywebView.setWebViewClient(new WebViewClient());
-        WebSettings settings = mywebView.getSettings();
-        settings.setDomStorageEnabled(true);
-        mywebView.loadUrl("https://clinic-management-system-fe.vercel.app/");
-        WebSettings webSettings=mywebView.getSettings();
+
+        webview =(WebView)findViewById(R.id.webview);
+
+
+
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://clinic-management-system-fe.vercel.app/");
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptThirdPartyCookies(webview,true);
+
+
+        webview.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webview.getSettings().setAppCacheEnabled(false);
+        webview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.getSettings().setBuiltInZoomControls(false);
+        webview.getSettings().setSupportZoom(false);
+        webview.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.getSettings().setDatabaseEnabled(true);
+        webview.getSettings().setDatabasePath("/data/data/" + webview.getContext().getPackageName() + "/databases/");
+
+        webview.setWebChromeClient(new WebChromeClient() {
+
+            // For api level bellow 24
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                if (url.startsWith("http")) {
+                    // Return false means, web view will handle the link
+                    return false;
+                }
+
+                return false;
+            }
+
+
+            // From api level 24
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                /*Toast.makeText(mcontext, "New Method",Toast.LENGTH_SHORT).show();*/
+
+                // Get the tel: url
+                String url = request.getUrl().toString();
+
+                if (url.startsWith("http")) {
+                    // Return false means, web view will handle the link
+                    return false;
+                }
+
+                return false;
+            }
+
+            // openFileChooser for Android 3.0+
+
+           /* public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                openImageChooser();
+            }*/
+
+            // For Lollipop 5.0+ Devices
+
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                mUploadMessages = filePathCallback;
+                openImageChooser();
+                return true;
+            }
+
+            // openFileChooser for Android < 3.0
+
+           /* public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                openFileChooser(uploadMsg, "");
+            }
+
+            //openFileChooser for other Android versions
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                openFileChooser(uploadMsg, acceptType);
+            }*/
+        });
+
     }
 
-    public class mywebClient extends WebViewClient{
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon){
-            super.onPageStarted(view,url,favicon);
-        }
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view,String url){
-            view.loadUrl(url);
-            return true;
+    private void openImageChooser() {
+        try {
+
+            File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "FolderName");
+
+            if (!imageStorageDir.exists()) {
+                imageStorageDir.mkdirs();
+            }
+            File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+            mCapturedImageURI = Uri.fromFile(file);
+
+            final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+
+            Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent});
+
+            startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
     @Override
-    public void onBackPressed(){
-        if(mywebView.canGoBack()) {
-            mywebView.goBack();
-        }
-        else{
-            super.onBackPressed();
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+
+            if (null == this.mUploadMessage && null == this.mUploadMessages) {
+                return;
+            }
+
+           /* Uri result;
+            if (requestCode != RESULT_OK){
+                result = null;
+            }else {
+                result = intent == null ? this.mCapturedImageURI : intent.getData();
+            }
+            this.mUploadMessage.onReceiveValue(result);
+            this.mUploadMessage = null;*/
+            if (null != mUploadMessage) {
+                handleUploadMessage(requestCode, resultCode, intent);
+
+            } else if (mUploadMessages != null) {
+                handleUploadMessages(requestCode, resultCode, intent);
+            }
         }
     }
+
+    private void handleUploadMessage(int requestCode, int resultCode, Intent intent) {
+        Uri result = null;
+        try {
+            if (resultCode != RESULT_OK) {
+                result = null;
+            } else {
+                // retrieve from the private variable if the intent is null
+
+                result = intent == null ? mCapturedImageURI : intent.getData();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mUploadMessage.onReceiveValue(result);
+        mUploadMessage = null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void handleUploadMessages(int requestCode, int resultCode, Intent intent) {
+        Uri[] results = null;
+        try {
+            if (resultCode != RESULT_OK) {
+                results = null;
+            } else {
+                if (intent != null) {
+                    String dataString = intent.getDataString();
+                    ClipData clipData = intent.getClipData();
+                    if (clipData != null) {
+                        results = new Uri[clipData.getItemCount()];
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            ClipData.Item item = clipData.getItemAt(i);
+                            results[i] = item.getUri();
+                        }
+                    }
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
+                    }
+                } else {
+                    results = new Uri[]{mCapturedImageURI};
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mUploadMessages.onReceiveValue(results);
+        mUploadMessages = null;
+    }
+
 }
